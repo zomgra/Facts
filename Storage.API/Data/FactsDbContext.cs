8af5dde9c9e9bf11f00b11d0fe7c0b6d77480ccf
Zomgra <hiban.com@gmail.com>
2023-07-31 22:43:15 +0200

    Добавьте файлы проекта.

---
+++ Storage.API/Data/FactsDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Storage.API.Data
+{
+    public class FactsDbContext : DbContext
+    {
+        public FactsDbContext(DbContextOptions<FactsDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Storage.API/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using Storage.API.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContextPool<FactsDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultString")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
